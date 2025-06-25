69e2c3ced2fe975e67e5b789442e4c8eb2a2a7e9
chetan mandalkar <you@example.com>
2025-06-25 14:55:21 +0530

    latest commit

--- blog/settings.py
+++ blog/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +30,7 @@ SECRET_KEY = 'django-insecure-nktrqcj_t%z-n%%)^s1&2e5du%awv6o2h%%%c&j$nnrq(0c$l_
 DEBUG = os.environ.get('DEBUG','False') == 'True'
 
 
-ALLOWED_HOSTS = ['blog-travel.onrender.com']
+ALLOWED_HOSTS = ['https://travel-blog-bf0d.onrender.com']
 
 
 # Application definition
@@ -78,11 +80,23 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url(
-        default = os.environ.get('DATABASE_URL')
-    )
-}
+# Database
+# Use SQLite locally if DATABASE_URL is not set
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
